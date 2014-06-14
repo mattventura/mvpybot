05cbab1378b67c43be10e9a811e97d9f3c67b95e
Matt Ventura <mattventura@mattventura.net>
2014-06-14 04:57:14 +0000

    Added pass and passwd to help, fixed help.py

--- modules/help.py
+++ modules/help.py
@@ -16,38 +16,47 @@ def register(r):
 	r.addhelp('register', help_register)
 	r.addhelp('modules', help_modules)
 	r.addhelp('reload', help_reload)
+	r.addhelp('pass', help_pass)
+	r.addhelp('passwd', help_passwd)
 
 def fmt(text):
 	return(text)
 
-def help_join():
+def help_join(cmd):
 	return fmt('The bot will join the specified channel. Usage: join #channel')
 
-def help_part():
+def help_part(cmd):
 	return fmt('The bot will part the specified channel. Usage: part #channel')
 
 
 
-def help_user():
+def help_user(cmd):
 	return fmt('Look up a user\'s information. Usage: user <username>. ')
 
-def help_auth():
+def help_auth(cmd):
 	return fmt('Authenticate using your username and password. Usage: auth <username> <password>. ')
 
 help_authenticate = help_auth
 
-def help_level():
+def help_level(cmd):
 	return fmt('Find your user level. This command takes no arguments. ')
 
-def help_deauth():
+def help_deauth(cmd):
 	return fmt('De-authenticate yourself. This command takes no arguments. ')
 
-def help_register():
+def help_register(cmd):
 	return fmt('Register a new account. Usage: register <username> <password>. ')
 
-def help_modules():
+def help_modules(cmd):
 	return fmt('Shows currently loaded modules. Modules that are loaded but disabled will still be shown.' )
 
-def help_reload():
+def help_reload(cmd):
 	return fmt('Reloads a module. Usage: reload <module>. ')
+
+def help_pass(cmd):
+	return fmt('Change your own password. Usage: pass <password>. ')
+
+def help_passwd(cmd):
+	return fmt('''Change an arbitrary user's password. Usage: pass <username> <password>. ''')
+
 
